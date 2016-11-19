d275e6c782a05b43e90498f1084601e77e431d22
David Crosby <darclaw.z@gmail.com>
2016-11-19 14:29:05 -0600

    stuff

---
+++ rth-derivitave/half-derivative.hs
@@ -0,0 +1,53 @@
+import Graphics.Gnuplot.Simple
+
+--data Dual = Dual Double Double deriving(Read, Show, Eq)
+--
+--constDual x = Dual x 0
+--
+--seedDual x x' = Dual x x'
+--
+--evalDual (Dual x _) = x
+--
+--diffDual (Dual _ x') = x'
+--
+--instance Num Dual where
+--   (+) (Dual u u') (Dual v v') = Dual (u+v) (u'+v')
+--   (*) (Dual u u') (Dual v v') = Dual (u*v) (u'*v+v'*u)
+--   (-) (Dual u u') (Dual v v') = Dual (u-v) (u'-v')
+--   negate (Dual u u') = Dual (negate u) (negate u')
+--   abs (Dual u u') = Dual (abs u) (u' * signum u)
+--   signum (Dual u u') = Dual (signum u) 0
+--   fromInteger n = Dual (fromInteger n) 0
+--
+--instance Fractional Dual where
+--   (/) (Dual u u') (Dual v v') = Dual (u/v) ((u'*v - u*v')/v**2)
+--   recip (Dual u u') = Dual (recip u) (-1*u'*(recip (u**2)))
+--   fromRational n = Dual (fromRational n) 0
+--
+--instance Floating Dual where
+--   pi = Dual pi 0
+--   exp (Dual u u') = Dual (exp u) (u'*exp u)
+--   sqrt (Dual u u') = Dual (sqrt u) (u' / (2*sqrt u))
+--   log (Dual u u') = Dual (log u) (u' / u)
+--   sin (Dual u u') = Dual (sin u) (u' * cos u)
+--   cos (Dual u u') = Dual (cos u) (- u' * sin u)
+--   tan (Dual u u') = Dual (tan u) (1/((cos u) ** 2))
+--   asin (Dual u u') = Dual (asin u) (u' / (sqrt(1-u**2)))
+--   acos (Dual u u') = Dual (acos u) (- u' / sqrt(1-u**2))
+--   atan (Dual u u')  = Dual (atan u) (u' / (1 + u ** 2))
+--   sinh (Dual u u')  = Dual (sinh u) (u' * cosh u)
+--   cosh (Dual u u')  = Dual (cosh u) (u' * sinh u)
+--   tanh (Dual u u')  = Dual (tanh u) (u' * (1 - (tanh u) ** 2))
+--   asinh (Dual u u') = Dual (asinh u) (u' / (sqrt(1 + u ** 2)))
+--   acosh (Dual u u') = Dual (acosh u) (u' / (sqrt(u ** 2 - 1)))
+--   atanh (Dual u u') = Dual (atanh u) (u' / (1 - u ** 2))
+--   (**) (Dual u u') (Dual v v')
+--     = Dual (u ** v) (u ** v * (v' * (log u) + (v * u' / u)))
+--   logBase (Dual u u') (Dual v v')
+--     = Dual (logBase u v) (((log v) * u' / u - (log u) * v' / v) / ((log u) ** 2))
+
+
+--plotFuncs [] (linearScale 1000 (-10,10)) [sin, cos]
+
+--taylors d@(Dual u u') x = taySum 0 d
+   --where taySum n (Dual u u') = x*
