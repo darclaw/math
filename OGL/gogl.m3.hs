bf9dba849bffe1afdd2a743bf178835973bb7b7b
David Crosby <darclaw.z@gmail.com>
2016-12-12 16:19:51 -0600

    found lots of oglg's

--- OGL/gogl.m3.hs
+++ OGL/gogl.m3.hs
@@ -3,6 +3,8 @@ import Control.Monad
 import qualified Data.Set as S
 import Data.Either
 import Debug.Trace
+import Control.Parallel
+import Control.Concurrent
 
 size = 3
 set = [0..(size-1)]
@@ -56,6 +58,12 @@ binComp uF f1 f2 = do
 
 funcToNum f = foldl (\acc (a,b,c) -> acc+c*size^(a*size+b)) 0 f
 
+numToFunc n = [(a,b,numToFunction n a b) | a<-set, b<-set]
+numToFunction n = (\x y -> ((take (size^2) (digs n))) !! (x*size+y))
+digs 0 = [0,0..]
+digs x = (x `mod` (size)) : (digs (x `div` size))
+
+listToFunc l= numToFunc $ sum (zipWith (\x y-> x*3^y) l [0..]) 
 
 treeComp uT@(EComp uF) t1@(EComp f1) t2@(EComp f2) = Comp (binComp uF f1 f2) uT t1 t2
 treeComp uT@(EComp uF) t1@(EComp f1) t2@(Comp f2 _ _ _) = Comp (binComp uF f1 f2) uT t1 t2
@@ -110,9 +118,9 @@ printList xs = mapM_ (\a -> do
 numToString n = [(['0'..'9']++['a'..'z']++['A'..'Z']++['ÿ'..]) !! n]
 
 --for 
---op = [ (0,0,2),(0,1,1),(0,2,1)
---      ,(1,0,0),(1,1,0),(1,2,1)
---      ,(2,0,0),(2,1,0),(2,2,1)]
+fop1 = [ (0,0,2),(0,1,1),(0,2,1)
+      ,(1,0,0),(1,1,0),(1,2,1)
+      ,(2,0,0),(2,1,0),(2,2,1)]
 --ct = treeComp opT (treeFng 0 0) (treeFng 2 1) 
 --dt = (treeComp ct (treeFng 2 0) (treeFng 1 0))
 --et = (treeComp dt (treeFng 1 1) (treeFng 0 1))
@@ -126,13 +134,18 @@ numToString n = [(['0'..'9']++['a'..'z']++['A'..'Z']++['ÿ'..]) !! n]
 --ht = (treeComp op (treeFng 1 2) op) -- in OGLG
 
 --for
---op = [ (0,0,2),(0,1,1),(0,2,0)
---      ,(1,0,1),(1,1,0),(1,2,2)
---      ,(2,0,2),(2,1,0),(2,2,1)]
+fop2 = [ (0,0,2),(0,1,1),(0,2,0)
+      ,(1,0,1),(1,1,0),(1,2,2)
+      ,(2,0,2),(2,1,0),(2,2,1)]
 --ct = treeComp opT (treeTrans opT) (opT)
 --dt = treeComp opT ct (treeTrans ct)
 --et = treeComp (treeTrans ct) (treeTrans ct) (treeTrans ct)
 --wantT = treeComp opT dt (treeRot 2 et)  -- in OGLG
+
+fop3 = numToFunc 5527
+fop4 = numToFunc 6049
+
+
 inEOGLG :: (Eq a, Num a) => [(a, a, a)] -> Bool
 --inEOGLG f = inEOGLGhelper f (extract (find (\(a,b,_) -> (a/=b)) f))
 --   where inEOGLGhelper [] c = True
@@ -152,24 +165,27 @@ inEOGLG f = let mDiag = [(a,b,c) | (a,b,c)<-f, a==b]
 
 allFuncs = [[ (0,0,a),(0,1,b),(0,2,c)
            ,(1,0,d),(1,1,e),(1,2,f)
-            ,(2,0,g),(2,1,h),(2,2,j)] |a<-set,b<-set,c<-set,d<-set,e<-set,f<-set,g<-set,h<-set,j<-set ]
+            ,(2,0,g),(2,1,h),(2,2,j)] |j<-set,h<-set,g<-set,f<-set,e<-set,d<-set,c<-set,b<-set,a<-set ]
 
-eoglg = S.fromList $ filter inEOGLG allFuncs
+eoglgpre = (filter inEOGLG allFuncs)++[fop1,fop2,fop3,fop4]
+eoglg = S.fromList (eoglgpre++(map trans eoglgpre))
 
 --funcs initialy contains op, ag, bg
 --checkInOGLG :: (Eq t, Eq t1, Eq t2) => [[(t,t1,t2)]]-> [b]
---n is iterations
+--n is iterations to run for, i is iterations ran
 --oglg is the set of functions known to be in oglg 
-checkInOGLG funcs oglg n 
+checkInOGLG funcs oglg i 0= Left (Nothing,0)
+checkInOGLG funcs oglg i n
   | any (`S.member` oglg) (S.fromList (map func funcs)) = Right ([(find (\t -> (func t) `S.member` oglg) funcs)],n)
+  | length funcs > 15 = Left (Nothing,0) -- prevents running to long
   | otherwise = --trace ("n ="++(show n)++" length funcs = "++(show (length funcs))) $
    let fs = [(f,fl,fr) | f<-funcs, fl<-funcs, fr<-funcs] 
     in let nfs = nubBy checkEqOrTrans [treeComp a b c | (a,b,c)<-fs]
-        in if any (inEOGLG) (map func nfs) then Right ([(find (\t -> inEOGLG (func t)) nfs)],n) else 
+        in --if any (inEOGLG) (map func nfs) then Right ([(find (\t -> inEOGLG (func t)) nfs)],n) else 
             -- (if all (`elem` (map func funcs)) (map func nfs) then (Nothing,n) else checkInOGLG nfs (n+1))
             -- (if all (`S.member` (S.fromList (map func funcs))) (map func nfs) then Left (Just funcs,n) else checkInOGLG nfs oglg (n+1))
             -- (if (S.fromList (map func funcs)) == (S.fromList (map func nfs)) then Left (Just funcs, n) else checkInOGLG nfs oglg (n+1))
-            if length funcs == (length nfs) then Left (Just funcs, n) else checkInOGLG nfs oglg (n+1)
+            if length funcs == (length nfs) then Left (Just funcs, n) else checkInOGLG nfs oglg (i+1) (n-1)
 
 --checkInOGLG'' funcs oglg n 
 --  | any (`S.member` oglg) (S.fromList (map func funcs)) = Right ([(find (\t -> (func t) `S.member` oglg) funcs)],n)
@@ -193,9 +209,9 @@ checkInOGLG funcs oglg n
 
 
 checkEqOrTrans t1 t2 
-   -- | (func t1)==ag && (func t2)==bg = False
-   -- | (func t1)==bg && (func t2)==ag = False
-   | otherwise = (func t1) == (func t2) -- || (func t1) == (binComp (func t2) bg ag) 
+   | (func t1)==ag && (func t2)==bg = False
+   | (func t1)==bg && (func t2)==ag = False
+   | otherwise = (func t1) == (func t2) || (func t1) == (binComp (func t2) bg ag) 
 
 sortDropTree [] = []
 sortDropTree [t] = [t]
@@ -205,14 +221,58 @@ sortDropTree (t:ts) = less++[t]++greater
 
 singleOGLG [] oglg = []
 singleOGLG (f:fs) oglg = 
-   let encV = checkInOGLG [EComp f] oglg 0
+   let encV = checkInOGLG [EComp f] oglg 0 (-1)
     in decide encV
    where decide (Right ([Just x],_)) = (f,x):(singleOGLG fs (f `S.insert` oglg ))
          decide (Left _) = singleOGLG fs oglg
 
 doubleOGLG [] oglg = []
 doubleOGLG ((f,g):fs) oglg = 
-   let encV = checkInOGLG [EComp f, EComp g] oglg 0
+   let encV = checkInOGLG [EComp f, EComp g] oglg 0 (-1)
     in decide encV
-   where decide (Right ([Just x],_)) = (f,g,x):(doubleOGLG fs (f `S.insert` oglg ))
+   where decide (Right ([Just x],_)) = (f,g,x):(doubleOGLG fs (f `S.insert` oglg )) --need to do something more than insert f oglg
          decide (Left _) = doubleOGLG fs oglg
+
+tripleOGLG [] oglg = []
+tripleOGLG ((f,g,h):fs) oglg = 
+   let encV = checkInOGLG [EComp f, EComp g, EComp h] oglg 0 (-1)
+    in decide encV
+   where decide (Right ([Just x],_)) = (f,g,h,x):(tripleOGLG fs (f `S.insert` oglg ))
+         decide (Left _) = tripleOGLG fs oglg
+
+findOGLG [] oglg d = []
+findOGLG (f:fs) oglg d = --trace ("f ="++(show (funcToNum f))) $
+   let encV = checkInOGLG [EComp f, agT, bgT] oglg 0 d
+    in decide encV
+   where decide (Right ([Just x],_)) = (f,x):(findOGLG fs (f `S.insert` oglg ) d)
+         decide (Left _) = findOGLG fs oglg d
+
+parFindOGLG [] oglg d= []
+parFindOGLG funcs@(f:fs) oglg d = 
+
+   let j = 120 --number of concurrent 
+    in 
+   let (jFs,restFs) = splitAt j funcs
+       in
+   let rjFs = map (\f -> findOGLG [f] oglg d) jFs
+       in
+   let x = foldl par (head rjFs) (rjFs) 
+       in
+   let y = (fmap fst (mconcat rjFs))
+       in
+   let noglg = oglg `S.union` (S.fromList y)
+       in
+   (y++(parFindOGLG restFs noglg d))
+   
+
+
+divides m n = rem n m == 0
+
+--primeFact::Int->[Int]
+factors n = foldl (\acc x -> if x `divides` n then x:acc else acc) [] [1..n]
+
+primeFact n = foldl (\acc x -> if length (factors x) == 2 then x:acc else acc) [] (factors n)
+
+main = do
+   let x = map (funcToNum) (parFindOGLG (allFuncs) eoglg (-1))
+   putStrLn (show x)
